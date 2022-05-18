ee8061f78f3c4a8c15f7033e53da8cb4c39cb269
connor <ConnorKirkpatrick3@gmail.com>
2022-05-18 14:43:56 +0100

    updated the server and client side codes to use the new methods

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const crypto = require("crypto");
-
 const path = require("path");
 const app = require("express")();
 const http = require("http").createServer(app);
@@ -7,7 +5,10 @@ const io = require("socket.io")(http);
 const express = require("express")
 app.use("/static", express.static(path.join(__dirname, "./static/")));
 
-const fs = require("fs")
+const ECDHE = require("./functions/ECDH/ECDHE")
+const encrypt = require("./functions/chacha/encrypt")
+const decrypt = require("./functions/chacha/decrypt")
+const masterKey = require("./functions/chacha/masterKey")
 
 let PORT = Number(process.env.PORT || 80);
 http.listen(PORT, () => {
@@ -17,3 +18,28 @@ http.listen(PORT, () => {
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/static/mainPage.html");
 });
+let Keys = ECDHE()
+console.log(Keys.getPublicKey())
+io.on("connection",(socket) => {
+    console.log("Client connected")
+    socket.emit("ServKey", (Keys.getPublicKey()))
+
+    socket.on("Test", () => {
+        console.log("Button clicked")
+    })
+
+    socket.on("PubKey", (CKey) =>{
+        console.log("Client key: ");
+        console.log(CKey)
+        let sharedSecret = Keys.computeSecret(CKey)
+        console.log(sharedSecret)
+        let data = "somedata"
+        let eData = encrypt(sharedSecret, data, data)
+        let encrypted = eData[0]
+        let nonce = eData[1]
+        let tag = eData[2]
+        console.log(encrypted)
+        console.log(decrypt(sharedSecret, encrypted, nonce, tag).toString())
+        //masterKey(sharedSecret, "")
+    })
+})
