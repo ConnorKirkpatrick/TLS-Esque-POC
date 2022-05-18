ad455f4f1f27ed4f5f96a29ee403c5f51c44c97c
connor <ConnorKirkpatrick3@gmail.com>
2022-05-18 17:23:36 +0100

    Added full handshake for the server and client pages server will send "test-Conn", client will respond with "good-Conn" if able to decrypt with the master key updated the readme with future ideas/plans

--- README.md
+++ README.md
@@ -4,7 +4,21 @@ This will be making use of a key exchange/negotiation protocol to exchange a pre
 We will use Epehmural ECDH for the key exchange protocol and AES-GCM or ChaCha20-Poly1305 for the cipher
 If successful and efficient, this project may be ported into other personal web-based projects for security
 
+flow:
+* server start
+* client connect
+* server queries for cookie
+  * if no cookie
+  * server generates new ECDH keys
+  * commands client to create new ECDH keys
+  * exchange of public keys to generate secret
+  * create entry with the master key
+  * server set cookie (timeout of 5 mins)
+  * all future messages to host are sent with this master key
+* if cookie
+  * find cookie entry that contains the master key for the connection
+  * decrypt message with master key
+
 https://www.npmjs.com/package/node-aes-gcm
 https://www.npmjs.com/package/chacha
-https://www.npmjs.com/package/eccrypto
 https://nodejs.org/docs/latest/api/crypto.html
--- main.js
+++ main.js
@@ -19,7 +19,7 @@ http.listen(PORT, () => {
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/static/mainPage.html");
 });
-
+let mKey = ""
 io.on("connection",(socket) => {
     console.log("Client connected")
     //upon connection, derive and send client the server public key
@@ -31,17 +31,10 @@ io.on("connection",(socket) => {
     })
     //receive the clients public key
     socket.on("PubKey", (CKey) =>{
-        console.log("Client key: ");
-        console.log(CKey)
         //generate shared secret from client and server keys
         let sharedSecret = Keys.computeSecret(CKey)
-        console.log("Shared secret: ")
-        console.log(sharedSecret)
         //generate a master password from the shared secret
-        let mKey = masterKey(sharedSecret, "")
-        console.log("Master Key: ")
-        console.log(mKey)
-        console.log(mKey.length)
+        mKey = masterKey(sharedSecret, "")
 
 
         let data = "conn-Test"
@@ -49,9 +42,19 @@ io.on("connection",(socket) => {
         let encrypted = eData[0]
         let nonce = eData[1]
         let tag = eData[2]
-        console.log(encrypted)
-        console.log(decrypt(mKey, encrypted, nonce, tag).toString())
-
+        socket.emit("newData", (eData))
+        //console.log(decrypt(mKey, encrypted, nonce, tag).toString())
+        
+        //Once encryption is created, attempt to communicate using the "new data" socket tag
+        //a second tag will be included inside the encrypted message to trigger actions
+    })
+    socket.on("newData", (eData) => {
+        if(decrypt(mKey, eData[0], eData[1], eData[2]).toString() === "good-Conn"){
+            console.log("Secure channel constructed")
+        }
+    })
+    socket.on("Bad-Conn", () => {
+        //resend data, or clear cookie and re-construct channel
     })
     //TODO Store ephemural key object, client cookie and master secret for connection once cipher is created
 })
