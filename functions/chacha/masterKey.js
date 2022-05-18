47ba30f14bba4d09af51400985bf08f9aef2e335
connor <ConnorKirkpatrick3@gmail.com>
2022-05-18 17:22:08 +0100

    updated masterKey for the server side

--- functions/chacha/masterKey.js
+++
@@ -1,7 +0,0 @@
-const crypto = require("crypto")
-function masterKey(key, salt){
-    let x = crypto.pbkdf2Sync(key,salt,20,256, 'sha512')
-    console.log(x.length)
-    console.log(x.toString())
-}
-module.exports = masterKey
