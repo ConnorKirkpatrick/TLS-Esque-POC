882121ae83da28f6ee07523575a44ca6e4cf8943
connor <ConnorKirkpatrick3@gmail.com>
2022-05-23 13:50:06 +0100

    Altered the storage of the client side key to allow for retention over page refreshes now makes use of window.localstorage rather than window.object

--- static/functions/newDataHandler.js
+++ static/functions/newDataHandler.js
@@ -1,7 +1,9 @@
 function newDataHandler(data){
     switch (data[0]){
         case "conn-Test":
-            socket.emit("clientMessage", (encrypt(window.mKey,"good-Conn")))
+            socket.emit("clientMessage", (encrypt(buffer.Buffer.from(Uint8Array.from(JSON.parse(window.localStorage.getItem("key")))),"good-Conn")))
             break
+        default:
+            console.log(data.toString())
     }
 }
