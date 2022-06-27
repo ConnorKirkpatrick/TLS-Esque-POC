ab68507794a1062c4c4e066709f36702e0b12842
Connor <ConnorKirkpatrick3@gmail.com>
2022-06-27 20:22:11 +0100

    Added socket event to inform server of cancellation

--- static/functions/generalFunctions/cancelNewClient.js
+++ static/functions/generalFunctions/cancelNewClient.js
@@ -1,4 +1,6 @@
 function cancelNewClient(){
     document.getElementById('cover').style.display = 'none'
     //send message to socket to terminate the new client request
+    let target = document.getElementById("cover-Text").innerText.substring(12,document.getElementById("cover-Text").innerText.indexOf(" to accept"))
+    socket.emit("clientMessage", (encrypt(buffer.Buffer.from(Uint8Array.from(JSON.parse(window.localStorage.getItem("key")))),"cancelRequest<SEPARATOR>"+target)))
 }
