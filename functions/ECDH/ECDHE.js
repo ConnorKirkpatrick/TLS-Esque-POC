162b5e478e69689f5548d0af3a5895d5eb116e89
Connor <ConnorKirkpatrick3@gmail.com>
2022-07-31 00:25:42 +0100

    Added JSDoc to ECDHE.js

--- functions/ECDH/ECDHE.js
+++ functions/ECDH/ECDHE.js
@@ -1,4 +1,11 @@
 const crypto = require("crypto")
+
+/**
+ * Elliptical Curve Diffe-Hellman Exchange key generator
+ * @returns {ECDH} ECDH public and private key container
+ * @constructor
+ */
+
 function ECDHE(){
     let keys = crypto.createECDH('secp256k1')
     keys.generateKeys()
