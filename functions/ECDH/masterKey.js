2d2522dffa3605fa2f778f33758ad5d156e9e581
Connor <ConnorKirkpatrick3@gmail.com>
2022-07-31 00:27:25 +0100

    Added JSDoc to masterKey.js

--- functions/ECDH/masterKey.js
+++ functions/ECDH/masterKey.js
@@ -1,4 +1,13 @@
 const crypto = require("crypto")
+
+/**
+ * Master Key Generator
+ * @param key {String/Buffer} Pre-master key material used to generate master key
+ * @param salt {String/Buffer} Salt for master key generation
+ * @returns {Buffer} Master key data contained inside a buffer
+ */
+
+
 function masterKey(key, salt){
     return crypto.pbkdf2Sync(key,salt,5,32, 'sha512')
 }
