74a60a85f615f55de5689dc2609e68cbb0e9f748
Connor <ConnorKirkpatrick3@gmail.com>
2022-07-30 23:48:46 +0100

    Added JSdoc to encrypt.js

--- functions/chacha/encrypt.js
+++ functions/chacha/encrypt.js
@@ -1,4 +1,11 @@
 const crypto = require("crypto")
+
+/**
+ * Cryptographical function used to encrypt a message using Chacha-poly1305
+ * @param key The symmetrical key for the encryption algorithm
+ * @param msg The message to be encrypted
+ * @returns {Buffer[]} The buffer containing the encrypted message information: The encrypted data, the Nonce and the tag
+ */
 function encrypt(key,msg){
     let nonce = crypto.randomBytes(12)
     let cipher = crypto.createCipheriv('chacha20-poly1305', key, nonce, { authTagLength: 16 });
