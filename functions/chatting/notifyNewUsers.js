94b16ccd8f0a6c30b9a6666dc8e5f4c5487ea25f
Connor <ConnorKirkpatrick3@gmail.com>
2022-07-31 00:22:59 +0100

    Added JSDoc to handshake.js

--- functions/chatting/notifyNewUsers.js
+++ functions/chatting/notifyNewUsers.js
@@ -1,7 +1,14 @@
 const encrypt = require("../chacha/encrypt");
 
+/**
+ * Alert active clients that a new client has joined and is available to message<br>
+ * @param nameMap {Array} Array of all active usernames
+ * @param io {Object} Socket.io object
+ * @param userMap {Map} Map containing all client information
+ */
+
 function notifyNewUsers(nameMap,io, userMap){
-    userMap.forEach((values,keys) => {
+    userMap.forEach((values,_) => {
         let data = "newUser<SEPARATOR>"+Array.from(nameMap)
         let eData = encrypt(values[0], data)
         io.to(values[3]).emit("serverMessage", (eData))
