810a3b0e4706d9ec82d4774c4fef88fe0c1ec093
Connor <ConnorKirkpatrick3@gmail.com>
2022-07-31 00:02:37 +0100

    Added JSdoc for forwardMessage.js

--- functions/chatting/forwardMessage.js
+++ functions/chatting/forwardMessage.js
@@ -1,9 +1,19 @@
 const encrypt = require("../chacha/encrypt");
 
+/**
+ * Server side function used to take message from a current client and send it to a targeted client <br>
+ * Requires locating target symmetrical key, re-encrypting the message  with target keys and then sending the message
+ * to the target
+ * @param {string} target Targeted client
+ * @param {string} msg The plaintext message to send to the target client
+ * @param {Map} userMap The Map of all active client information
+ * @param {Object} io Socket.io Object
+ */
+
 function forwardMessage(target, msg, userMap, io){
     //identify which socket the target is on
     //encrypt the message with the users key, send the message privately
-    userMap.forEach((values,keys) => {
+    userMap.forEach((values,_) => {
         if(values[2] === target){
             let data = "newMessage<SEPARATOR>"+msg
             let eData = encrypt(values[0], data)
