3c9db15c48f55eef04b1604a9018c75d7b334e38
Connor <ConnorKirkpatrick3@gmail.com>
2022-07-31 00:09:42 +0100

    Updated JSDoc to include types

--- functions/chacha/decrypt.js
+++ functions/chacha/decrypt.js
@@ -2,10 +2,10 @@ const crypto = require("crypto")
 
 /**
  * Cryptographical function to decrypt a chacha-poly1305 message
- * @param key The symmetrical key for the encryption
- * @param msg The encrypted message
- * @param nonce The Nonce of the encrypted message
- * @param tag The tag of the encrypted message
+ * @param {Buffer} key The symmetrical key for the encryption
+ * @param {String} msg The encrypted message
+ * @param {Buffer} nonce The Nonce of the encrypted message
+ * @param {Buffer} tag The tag of the encrypted message
  * @returns {Buffer} A buffer containing the decrypted message information
  */
 
