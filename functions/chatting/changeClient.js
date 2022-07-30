5b294d4e5bc4852bc52747cc7857840047ba6a1c
Connor <ConnorKirkpatrick3@gmail.com>
2022-07-30 23:52:15 +0100

    Added JSdoc to changeClient.js

--- functions/chatting/changeClient.js
+++ functions/chatting/changeClient.js
@@ -1,7 +1,16 @@
 const encrypt = require("../chacha/encrypt");
 
+/**
+ * Function used manage requesting, accepting and denying message requests between users
+ * @param uName The username of the client sending the message
+ * @param userMap The usermap containing information on all clients
+ * @param target The target user for the message request information
+ * @param io Socket.io object
+ * @param flag Flag identifying which kind of message the user wishes to send the target
+ */
+
 function changeClient(uName, userMap, target, io, flag){
-    userMap.forEach((values,keys) => {
+    userMap.forEach((values,_) => {
         if(values[2] === target){
             let data = ""
             switch (flag){
