d3659c32bf7b022d0aefeaeded820ef16afa3968
Connor <ConnorKirkpatrick3@gmail.com>
2022-07-30 23:46:42 +0100

    Added jsdoc to decrypt.py

--- functions/chacha/decrypt.js
+++ functions/chacha/decrypt.js
@@ -1,4 +1,14 @@
 const crypto = require("crypto")
+
+/**
+ * Cryptographical function to decrypt a chacha-poly1305 message
+ * @param key The symmetrical key for the encryption
+ * @param msg The encrypted message
+ * @param nonce The Nonce of the encrypted message
+ * @param tag The tag of the encrypted message
+ * @returns {Buffer} A buffer containing the decrypted message information
+ */
+
 function decrypt(key,msg,nonce, tag){
     let decipher = crypto.createDecipheriv('chacha20-poly1305', key, nonce, { authTagLength: 16 });
     let decrypted = decipher.update(msg)
