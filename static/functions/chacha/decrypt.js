cbe2793ec68e28150c328aab4ffd81168eded91b
Connor <ConnorKirkpatrick3@gmail.com>
2022-07-31 22:48:11 +0100

    Added JSDoc to client side decrypt.js

--- static/functions/chacha/decrypt.js
+++ static/functions/chacha/decrypt.js
@@ -1,3 +1,11 @@
+/**
+ * Decryption function for client side ChaCha-Poly1305 encryption
+ * @param {Buffer} key Symmetrical encryption key
+ * @param {Buffer} msg Encrypted message buffer
+ * @param {Buffer} nonce Nonce of the encrypted message
+ * @param {Buffer} tag Tag of the encrypted message
+ * @returns {Buffer} Decrypted message text
+ */
 function decrypt(key,msg,nonce, tag){
     let decipher = chacha.createDecipher(key, buffer.Buffer.from(nonce))
     let decrypted = decipher.update(buffer.Buffer.from(msg))
