af2358185c8228b0fedec70ad9c05885d557d0d0
Connor <ConnorKirkpatrick3@gmail.com>
2022-07-31 22:54:26 +0100

    Added JSDoc to client side ECDHE.js

--- static/functions/ECDH/ECDHE.js
+++ static/functions/ECDH/ECDHE.js
@@ -1,6 +1,9 @@
+/**
+ * Small ECDH function used to produce the client side asymmetrical keys
+ * @returns {Object} Crypto object containing the keys, use getter to obtain
+ * @constructor
+ */
 function ECDHE(){
     window.keys = cryptob.createECDH('secp256k1')
     return keys.generateKeys()
 }
-
-///browserify .\ECDHE.js -o -s module > bundle.js
