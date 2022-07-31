319581a6965a21bb714cb9a964fb03c61d45ab8f
Connor <ConnorKirkpatrick3@gmail.com>
2022-07-31 22:50:58 +0100

    Added JSDoc to client side encrypt.js

--- static/functions/chacha/encrypt.js
+++ static/functions/chacha/encrypt.js
@@ -1,3 +1,9 @@
+/**
+ * Client side encryption function for ChaCha-poly1305 encryption
+ * @param {Buffer} key Symmetrical key used to encrypt the message
+ * @param {String} msg Message to encrypt
+ * @returns {Buffer[]} Array of Buffers containing: The encrypted message, the message Nonce, the message tag
+ */
 function encrypt(key,msg){
     let nonce = cryptob.randomBytes(12)
     let cipher = chacha.createCipher(key, buffer.Buffer.from(nonce))
