a1675614165d0c3ac46ab6b5a98cd6e391fb5f48
Connor <ConnorKirkpatrick3@gmail.com>
2022-07-31 23:00:05 +0100

    Added JSDoc to client side SecGen.js

--- static/functions/ECDH/SecGen.js
+++ static/functions/ECDH/SecGen.js
@@ -1,3 +1,8 @@
+/**
+ * Tiny function used to generate pre-master secret from ECDH exchanged keys
+ * @returns {Buffer} Pre-Master secret
+ * @constructor
+ */
 function SecGen(){
     return window.Secret = window.keys.computeSecret(window.ServKey)
 }
