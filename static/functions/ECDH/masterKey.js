f3accd1268ee2c8fff6e275bb50b6b45494b5817
Connor <ConnorKirkpatrick3@gmail.com>
2022-07-31 22:56:00 +0100

    Added JSDoc to client side masterKey.js

--- static/functions/ECDH/masterKey.js
+++ static/functions/ECDH/masterKey.js
@@ -1,3 +1,9 @@
+/**
+ * Client side master key generator, generates key from pre-master secret material
+ * @param {Buffer} key Buffer containing pre-master key material
+ * @param {String} salt Salt used in generation of the master secret
+ * @returns {Buffer} Master key material
+ */
 function masterKey(key, salt){
     return cryptob.pbkdf2Sync(key,salt,5,32,'sha512')
 }
